f884a52e6a635aac5f3fe40277c19da76f5eb698
Thien Ong <167042459+ThienNhanOng@users.noreply.github.com>
2026-02-23 17:58:58 -0500

    icon and working menu

--- movie-manager/src/components/navbar.jsx
+++ movie-manager/src/components/navbar.jsx
@@ -2,26 +2,27 @@ import React from "react";
 
 export default function Navbar() {
   return (
-
-    // suppose to be the hamburger menu but isnt showing
-    <div className="navbar bg-base-100 shadow-sm">
-      <div className="flex-none">
-        <button className="btn btn-square btn-ghost" aria-label="menu">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block h-5 w-5 stroke-current">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
+    <div className="navbar bg-base-100 shadow-sm flex items-center px-4 py-2 border border-gray-300 bg-gray-100 rounded-lg">
+      {/* Logo*/}
+      <div className="flex-none flex flex-col items-center mr-4">
+        <button className="btn btn-circle flex flex-col items-center justify-center h-16 w-16" aria-label="logo">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2.5" stroke="currentColor" className="size-[1.8em]">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
           </svg>
+          <img src="/movie-manager-icon.png" alt="Logo" className="h-11 w-11 mt-1 object-contain" style={{maxWidth:'52px',maxHeight:'52px'}} />
         </button>
       </div>
 
-    {/* header name button with daisyui design */}
-      <div className="flex-1 flex items-center justify-start pl-4">
-        <button className="btn btn-ghost text-xl btn-wide w-56">Movie Manager</button>
-      </div>
-      <div className="flex-none">
+      <div className="flex-1" />
+      {/* Hamburger */}
+      <div className="flex-none ml-auto">
         <details className="dropdown dropdown-end">
-        {/* droplist */}
-          <summary className="btn m-1 pr-4">Droplist</summary>
-          <ul className="menu dropdown-content bg-base-100 rounded-box z-10 w-52 p-2 shadow-sm right-0">
+          <summary className="btn btn-square btn-ghost" aria-label="menu">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block h-5 w-5 stroke-current">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
+            </svg>
+          </summary>
+          <ul className="menu dropdown-content bg-base-100 rounded-box z-10 w-36 p-2 shadow-sm mt-2 right-0">
             <li>
               <button className="btn btn-wide px-4 py-2 mb-2">Wish List</button>
             </li>
@@ -30,15 +31,6 @@ export default function Navbar() {
             </li>
           </ul>
         </details>
-      </div>
-
-      {/* ... menu was going to add sign in option here. not showing either */}
-      <div className="flex-none">
-        <button className="btn btn-square btn-ghost" aria-label="more">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block h-5 w-5 stroke-current">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path>
-          </svg>
-        </button>
       </div>
     </div>
   );
