33163a1ec8e430e3e4d166a5ba178d36b227157b
ibowers27 <i.bowers27@ncf.edu>
2026-02-26 23:54:13 -0500

    fix case-sensative imports

--- movie-manager/src/App.jsx
+++ movie-manager/src/App.jsx
@@ -11,8 +11,8 @@ import useMovieFilters from "./hooks/MovieFilters";
 
 // Components
 import Header from "./components/header";
-import FilterBar from "./components/FilterBar";
-import MovieList from "./components/MovieList";
+import FilterBar from "./components/Filterbar";
+import MovieList from "./components/movielist";
 import BodyPiece from "./components/body";
 import Footer from "./components/footer";
 
